dc8ad475ce5dee33a753591ba08fdb49198c92b6
aj-800 <43896103+aj-800@users.noreply.github.com>
2019-03-09 18:42:54 +1300

    Correct attribute

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -9,9 +9,12 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        [Required]
+        
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public int? OrbitedObjectId { get; set; }
 
         [NotMapped]
